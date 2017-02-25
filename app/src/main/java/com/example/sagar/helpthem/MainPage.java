d7fbc6b08c30fece5fff67c8ada0c09c872e3385
Sagar Garg <sagargarg@gmail.com>
2017-02-25 11:50:55 -0500

    Link main page to activity page

--- app/src/main/java/com/example/sagar/helpthem/MainPage.java
+++ app/src/main/java/com/example/sagar/helpthem/MainPage.java
@@ -1,7 +1,10 @@
 package com.example.sagar.helpthem;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainPage extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        RelativeLayout rlayout = (RelativeLayout) findViewById(R.id.main_page);
+        rlayout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainPage.this, QuestionPage.class);
+                startActivity(intent);
+            }
+
+        });
     }
+
 }
